75b10334c7b8288471cf4db0d3668c213bc29782
ccortesp89 <62478671+ccortesp89@users.noreply.github.com>
2021-04-22 01:20:51 -0400

    parte 1

---
+++ js/businessLogic.js
@@ -0,0 +1,17 @@
+var friendList = [];
+
+
+
+function addFriendToSystem(pid, pname, pcountry, pbirthday, pemail){
+
+    var newFriend = {
+        id : pid,
+        name : pname,
+        country : pcountry,
+        birthday : pbirthday,
+        email : pemail
+    };
+    console.log(newFriend);
+    friendList.push(newFriend);
+
+}
---
+++ js/uiLogic.js
@@ -0,0 +1,11 @@
+document.querySelector('#btnSave').addEventListener('click', saveFriend);
+
+function saveFriend(){
+    var sId = document.querySelector('#txtId').value,
+        sName = document.querySelector('#txtName').value,
+        sCountry = document.querySelector('#txtCountry').value,
+        sBirthdat = document.querySelector('#txtBirthday').value,
+        sEmail = document.querySelector('#txtEmail').value;
+
+    addFriendToSystem(sId, sName, sCountry, sBirthdat, sEmail);
+}
